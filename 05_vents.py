d798d37fa1b4d76d87ad39ce45023891c33070f4
Toofty5 <jay.cheung@gmail.com>
2021-12-05 10:33:45 -0500

    Day 5 complete

--- 05_vents.py
+++ 05_vents.py
@@ -1,51 +1,49 @@
+import numpy as np
+
 lines = [line.split() for line in open("input", "r")]
 
 segments = []
 
 for line in lines:
-    x1,y1 = line[0].split(",")
-    x2,y2 = line[2].split(",")
+    x1,y1 = map(int,line[0].split(","))
+    x2,y2 = map(int,line[2].split(","))
 
-    if x1 > x2:
+    if x1 > x2:   
         x1,x2 = x2,x1
-    if y1 > y2:
+        y1,y2 = y2,y1
+    if x1 == x2 and y2 < y1:
         y1,y2 = y2,y1
 
-    segments.append((int(x1),int(y1), int(x2), int(y2)))
+    segments.append((x1,y1,x2,y2))
 
+total = 0 
+grid = np.zeros([1000,1000])
+print(grid)
 
-x_dict = {} 
-y_dict = {} 
+for x1,y1,x2,y2 in segments:
 
-total = 0
-
-for (x1,y1,x2,y2) in segments:
-
-    if x1 == x2:
-        total += abs(y1-y2)
-        if x1 in x_dict:
-            x_dict[x1].append((y1,y2))
-            print(x_dict[x1])
-        else:
-            x_dict[x1] = [(y1,y2)]
-    else: 
-        total += abs(x1-x2)
-        if y1 in y_dict:
-            y_dict[y1].append((x1,x2))
-        else:
-            y_dict[y1] = [(x1,x2)]
-print(total)
-#find crosses
-for x_key in x_dict:
-    segments = x_dict[x_key]
-    
-    for y_key,y_segs in y_dict.items():
-        for (y1,y2) in segments:
-            if y1 <= y_key <= y2:
+    if x1==x2: # vertical 
+        for y in range(y1,y2+1):
+            if grid[x1,y] == 1:
                 total += 1
-                break
+            grid[x1,y] += 1
 
+    elif y1==y2: # horizontal
+        for x in range(x1,x2+1):
+            if grid[x,y1] == 1:
+                total += 1
+            grid[x,y1] += 1
+
+    else:
+        for dx in range(x2-x1+1):
+            if y1>y2:
+                dy = -dx
+            else:
+                dy = dx
+
+            if grid[x1+dx,y1+dy] == 1:
+                total += 1
+            
+            grid[x1+dx , y1+dy] += 1
 
 print(total)
-print(x_dict)
-        

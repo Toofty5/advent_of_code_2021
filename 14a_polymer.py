46d42cd190b3083d110e69f3669b9b70f2718591
Toofty5 <jay.cheung@gmail.com>
2021-12-14 12:20:27 -0500

    Day 14 complete

--- 14a_polymer.py
+++ 14a_polymer.py
@@ -6,18 +6,43 @@ template = [char for char in lines[0]]
 
 split_lines = [line.split(" -> ") for line in lines[2:]]
 pair_dict = {(a[0],a[1]):b for a,b in split_lines}
+zipped = list(zip(template, template[1:]))
+
+poly_dict = {}
+counts = {}
+
+def d_add(d, pair, count=1):
+    if pair in d:
+        d[pair] += count 
+    else:
+        d[pair] = count 
+
+def d_sub(d,pair, count=1):
+    d[pair] -= count
+    if d[pair] == 0:
+        d.pop(pair)
+
+for e in template:
+    d_add(counts, e)
+
+for pair in zipped:
+    d_add(poly_dict, pair)
 
 
+for i in range(40):
+    new_poly = poly_dict.copy()
+    for a,b in poly_dict:
+        count = poly_dict[(a,b)]
+        new_element = pair_dict[(a,b)]
+        d_add(new_poly, (a,new_element),count)
+        d_add(new_poly, (new_element,b),count)
+        d_sub(new_poly, (a,b),count)
+        d_add(counts, new_element, count = count)
+    poly_dict = new_poly
 
-for i in range(10):
-    print(i)
-    pairs = zip(template, template[1:])
-    triplets = [[a, pair_dict[(a,b)] ,b] for (a,b) in pairs]
-    template = list(it.chain(triplets[0], *[t[1:] for t in triplets[1:]]))
-    
+    for thing in poly_dict.items():
+        print(thing)
+    print(counts)
 
-
-elements = set(template)
-for i in elements:
-    print(i, template.count(i))
-breakpoint()
+vals = counts.values()
+print(max(vals), min(vals), max(vals)-min(vals))

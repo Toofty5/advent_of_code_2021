27edf32a7e5830e542cb1a7aeb89870f945a70af
Toofty5 <jay.cheung@gmail.com>
2022-01-08 18:02:10 -0500

    Finding a shorter path needs to trigger reevaluation

--- 15a_chiton.py
+++ 15a_chiton.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-topo_array = np.array([[int(char) for char in line.strip()] for line in open("input","r").readlines()])
+topo_array = np.array([[int(char) for char in line.strip()] for line in open("input2","r").readlines()])
 NUM_ROWS, NUM_COLS = topo_array.shape
 
 cost_array = np.zeros(topo_array.shape)
@@ -9,13 +9,14 @@ cost_array.fill(INF)
 
 
 def main() :
-    start_node = (NUM_ROWS-1, NUM_COLS-1)
-    cost_array[start_node] = topo_array[start_node]
+    #start_node = (NUM_ROWS-1, NUM_COLS-1)
+    start_node = (0,0)
+    cost_array[start_node] = 0
 
     queue = [start_node]
 
     while queue != []:
-        
+        #print(np.array(list(zip(topo_array[:10,:10], cost_array[:10,:10]))))
         this_node = queue.pop(0)
         queue.extend(unvisited_neighbors(this_node))
 
@@ -23,8 +24,6 @@ def main() :
             build_topo(next_node, cost_array[this_node])
     print(topo_array)
     print(cost_array)
-    print(topo_array.shape)
-    print("don't forget to subtract the cost of the first node!")
 
 def neighbors(this_node):  # return VISITED neighbors
     this_row,this_col = this_node
@@ -43,35 +42,15 @@ def neighbors(this_node):  # return VISITED neighbors
         right = this_row, this_col + 1
         neighbors.append(right)
 
-    neighbors.sort(key = lambda x: topo_array[x]) 
+    #neighbors.sort(key = lambda x: topo_array[x]) 
     return neighbors
 
 def unvisited_neighbors(this_node):  # return UNVISITED neighbors
-    this_row,this_col = this_node
-    neighbors = []
-
-    if this_row > 0: 
-        up = (this_row - 1, this_col)
-        if cost_array[up] == INF:
-            neighbors.append(up)
-    if this_row < NUM_ROWS - 1:
-        down = (this_row + 1, this_col)
-        if cost_array[down] == INF:
-            neighbors.append(down)
-    if this_col > 0:
-        left = (this_row, this_col - 1)
-        if cost_array[left] == INF:
-            neighbors.append(left)
-    if this_col < NUM_COLS - 1:
-        right = this_row, this_col + 1
-        if cost_array[right] == INF:
-            neighbors.append(right)
-
-    neighbors.sort(key = lambda x: topo_array[x]) 
-    return neighbors
+    return [nei for nei in neighbors(this_node) if cost_array[nei] == INF]
 
 
-def build_topo(this_node, cost_so_far):
+# replace item in cost_array if new cost is cheaper.
+def build_topo(this_node, cost_so_far): 
     new_cost = topo_array[this_node] + cost_so_far
     if(cost_array[this_node] > new_cost):
         cost_array[this_node] = new_cost

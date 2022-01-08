22e76d950d336a8a8efcf2d09cca00b1b842ebb0
Toofty5 <jay.cheung@gmail.com>
2022-01-08 15:09:54 -0500

    Need to implement breadth first traversal

--- 15a_chiton.py
+++ 15a_chiton.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-val_arr = np.array([[int(char) for char in line.strip()] for line in open("input","r").readlines()])
-NUM_ROWS, NUM_COLS = val_arr.shape
+topo_array = np.array([[int(char) for char in line.strip()] for line in open("input","r").readlines()])
+NUM_ROWS, NUM_COLS = topo_array.shape
 
-topo_array = np.zeros(val_arr.shape)
-INF = 9999
-topo_array.fill(INF)
-print(val_arr)
+cost_array = np.zeros(topo_array.shape)
+INF = 999
+cost_array.fill(INF)
+
+print(topo_array)
 
 
 
@@ -14,35 +15,48 @@ def main() :
     start_node = (NUM_ROWS-1, NUM_COLS-1)
 
     build_topo(start_node, 0)
-    print(topo_array)
 
+    print(cost_array)
 
-def build_topo(this_node, cost_so_far):
-    new_cost = val_arr[this_node] + cost_so_far
-    topo_array[this_node] = new_cost
+def get_neighbors(this_node):  # return UNVISITED neighbors
     
-
     this_row,this_col = this_node
     neighbors = []
 
     if this_row > 0: 
         up = (this_row - 1, this_col)
-        neighbors.append(up)
+        if cost_array[up] == INF:
+            neighbors.append(up)
     if this_row < NUM_ROWS - 1:
         down = (this_row + 1, this_col)
-        neighbors.append(down)
+        if cost_array[down] == INF:
+            neighbors.append(down)
     if this_col > 0:
         left = (this_row, this_col - 1)
-        neighbors.append(left)
+        if cost_array[left] == INF:
+            neighbors.append(left)
     if this_col < NUM_COLS - 1:
         right = this_row, this_col + 1
-        neighbors.append(right)
+        if cost_array[right] == INF:
+            neighbors.append(right)
 
-    neighbors.sort(key = lambda x: val_arr[x]) 
+    return neighbors
+
+
+def build_topo(this_node, cost_so_far):
+    new_cost = topo_array[this_node] + cost_so_far
+    cost_array[this_node] = new_cost
+
+    neighbors = get_neighbors(this_node)
+    
+    neighbors.sort(key = lambda x: topo_array[x]) 
+    
+    if neighbors ==[]:
+        return
 
     for nei in neighbors:
-        if topo_array[nei] == INF:
-            build_topo(nei, new_cost)
+        cost_array[nei] = new_cost + topo_array[nei]
+
     
 if __name__ == "__main__" :
     main()

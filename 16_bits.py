1446e5dc65aa52a61c1c003b45efa430b9ce6be3
Toofty5 <jay.cheung@gmail.com>
2022-01-10 08:10:40 -0500

    Starting on day 16

--- 16_bits.py
+++ 16_bits.py
@@ -1,20 +1,42 @@
-hex_in = [int(char, 16) for char in open("input","r").readline().strip()]
-bin_in = ''.join([format(digit, '04b') for digit in hex_in])
+import textwrap
+
+
+class BitStream:
+    def __init__(self, bits):
+        self.ptr = 0
+        self.bits = bits
+    
+    #returns next num bits without moving pointer
+    def next(self, num = 1): 
+        return bits[ptr: ptr+num]
+    
+    #returns num bits and moves pointer
+    def read(self, num):
+        output = bits[ptr: ptr+num] if ptr + num < len(self.bits) else bits[ptr:]
+
+        ptr += num
+        return output
+
+    def __str__(self):
+        return self.bits
+
+    def __int__(self):
+        return int(self.bits, 2)
 
 
 
 def main():
-    print(bin_in)
-    print(packetize(bin_in))
+    hex_in = [int(char, 16) for char in open("input","r").readline().strip()]
+    bin_in = ''.join([format(digit, '04b') for digit in hex_in])
+
+    stream = BitStream(bin_in)
+    print(stream)
+
+    print(int(stream))
 
 
 
 #return list of tuples
-def packetize(in_str):
-    pkt_ver = int(in_str[:3], 2)
-    pkt_type = int(in_str[3:6], 2)
-
-    return (pkt_ver,pkt_type)
 
 if __name__ == "__main__":
     main()
